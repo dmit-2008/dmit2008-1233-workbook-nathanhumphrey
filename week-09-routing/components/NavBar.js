b943528bc9c66aec80f43f1c0533622566cf4c2c
Nathan Humphrey <NathanH@nait.ca>
2024-07-04 18:28:29 -0600

    Update week 09 navigation - fix NavBar flex

--- week-09-routing/components/NavBar.js
+++ week-09-routing/components/NavBar.js
@@ -7,25 +7,27 @@ export default function NavBar() {
 
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Box>
           <Link href="/">
             <Typography variant="h6" component="div">
               Page Navigation Demo
             </Typography>
           </Link>
         </Box>
-        <Link href="/about">
-          <Button color="inherit">About</Button>
-        </Link>
-        <Button
-          color="inherit"
-          onClick={() => {
-            router.push(`/folder/${Math.random()}`);
-          }}
-        >
-          Random
-        </Button>
+        <Box>
+          <Link href="/about">
+            <Button color="inherit">About</Button>
+          </Link>
+          <Button
+            color="inherit"
+            onClick={() => {
+              router.push(`/folder/${Math.random()}`);
+            }}
+          >
+            Random
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
