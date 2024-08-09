4d345d00986b7478219839178bacbabb2e8d86d1
Nathan Humphrey <nathpaulhump@gmail.com>
2024-08-08 18:55:35 -0600

    Update week 14 Firebase Auth

--- week-14-next-firebase-auth/context/AuthContext.js
+++ week-14-next-firebase-auth/context/AuthContext.js
@@ -1,15 +1,41 @@
 import { createContext, useState } from 'react';
+import { auth } from '@/firebase/config';
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
 
 const defaultValue = {
-  email: 'jdoe@example.com',
-  uid: '123',
-  role: 'user',
+  user: null,
+  isReady: false,
+  signIn: async function (email, password) {
+    try {
+      const cred = await signInWithEmailAndPassword(auth, email, password);
+      const user = cred.user;
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(`Error signing in: ${errorCode} - ${errorMessage}`);
+    }
+  },
+  signOut: async function () {
+    await signOut();
+  },
 };
 
 export const AuthContext = createContext({ user: defaultValue });
 
 const AuthProvider = ({ children }) => {
   const [userState, setUserState] = useState(defaultValue);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUserState(user);
+    } else {
+      setUserState(null);
+    }
+  });
 
   return (
     <AuthContext.Provider value={{ user: userState, setUser: setUserState }}>
